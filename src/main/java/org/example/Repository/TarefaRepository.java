fc11fdc7ceff8db325f6fcb09b4c23afff40d8d9
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-04-04 22:11:30 -0300

    atualização do serviço de excluir categoria (atrbiuindo null às tarefas que estiverem contidas na categoria excluída)

--- src/main/java/org/example/Repository/TarefaRepository.java
+++ src/main/java/org/example/Repository/TarefaRepository.java
@@ -3,11 +3,15 @@ package org.example.Repository;
 import org.example.Model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa,Long> {
 
     boolean existsByDescricao(String descricao);
     boolean existsByDescricaoAndIdNot(String descricao, Long id);
+    List<Tarefa> findByCategoriaId(Long categoriaId);
 
 }
--- src/main/java/org/example/Services/Categoria/ExcluirCategoriaService.java
+++ src/main/java/org/example/Services/Categoria/ExcluirCategoriaService.java
@@ -1,10 +1,14 @@
 package org.example.Services.Categoria;
 
 import org.example.Model.Categoria;
+import org.example.Model.Tarefa;
 import org.example.Repository.CategoriaRepository;
+import org.example.Repository.TarefaRepository;
 import org.example.Utils.CategoriaNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ExcluirCategoriaService {
@@ -12,10 +16,18 @@ public class ExcluirCategoriaService {
     @Autowired
     CategoriaRepository categoriaRepository;
 
+    @Autowired
+    TarefaRepository tarefaRepository;
+
     public String executar(Categoria categoria){
         if(!categoriaRepository.existsById(categoria.getId())){
             throw new CategoriaNotFoundException();
         }
+        List<Tarefa> tarefas = tarefaRepository.findByCategoriaId(categoria.getId());
+        tarefas.forEach(tarefa -> {
+            tarefa.setCategoria(null);
+            tarefaRepository.save(tarefa);
+        });
         categoriaRepository.delete(categoria);
         return ("Categoria deletada com sucesso.");
     }
