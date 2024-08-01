37eb23a857cf015a221b129bb06a577f8be4df84
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-08-01 16:52:13 -0300

    correção de import

--- src/main/java/org/example/Repository/CategoriaRepository.java
+++ src/main/java/org/example/Repository/CategoriaRepository.java
@@ -1,7 +1,6 @@
 package org.example.Repository;
 
 import org.example.Model.Categoria;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/org/example/Repository/TarefaRepository.java
+++ src/main/java/org/example/Repository/TarefaRepository.java
@@ -3,7 +3,6 @@ package org.example.Repository;
 import org.example.Model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 
